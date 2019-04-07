1364aa6365422e5c50a72cfa16bc67126827f4d3
engaymanh <eng.aymanhariri@gmail.com>
2019-04-07 19:08:09 +0300

    Refactor

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,42 +4,39 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  var str = "";
-    // check obj if it array
-  	if ( Array.isArray(obj)) {
-	   	str += "[";
-	    
-	    for ( var k in obj ) {
-	      str += stringifyJSON(obj[k])  ;
-	      if ( k < obj.length - 1 ){
-	      	str += ",";
-	      }
-	    }
-	    str += "]";
-      // check if obj string, number, null, undefined, boolean 
-  	} else if ( typeof obj !== "object" || obj === null){
 
-  	  	if ( typeof obj === "string" ){
-      		str += "\"" + obj + "\"";
-      	} else { 
-      	 str += obj;
-        }  
-        // if obj is object
-    } else {
-
-        str += "{";
-        for ( var k in obj ){
-        	if ( obj[k] === undefined ||  typeof obj[k] === "function" ){
-  					str += "";
-  			} else {
-      	  		str += "\"" + k + "\":" + stringifyJSON( obj[k] )  ;
-       			if ( k != Object.keys( obj )[Object.keys( obj ).length-1] ){
-      				str += ","
-          		}   
-        	}
-      	}
-        str += "}";
+//stringify Array
+  function stringifyArr(arr){
+    var str = [];
+      for ( var k in obj ) {
+        //recursion call for every element in the array
+        str.push(stringifyJSON(obj[k])) ;
       }
-      
-  return str;
+    return "[" + str.join(",") + "]";
+  }
+
+  //stringify object
+  function stingifyObj(object){
+    var str = [];
+    for ( var k in obj ){
+      if ( obj[k] === undefined ||  typeof obj[k] === "function" ){
+        break;
+      } 
+      //recursion call for every element in the object key and value
+      str.push(stringifyJSON(k) + ":" + stringifyJSON( obj[k] ));
+    } 
+    return "{" + str.join(",") + "}";
+  }
+
+/// the main process deben on the type of obj 
+  if ( Array.isArray(obj)) {
+    return stringifyArr(obj);
+  } else if(typeof obj === "object" && obj !== null){
+    return stingifyObj(obj);
+  } else if ( typeof obj === "string" ){
+    return "\"" + obj + "\"";
+   // check if obj  number, null, undefined, boolean        
+	} else {
+    return obj +"";
+  }  	 
 };
