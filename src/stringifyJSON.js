70addb2036334db43e886e9a325e92dea0718f8d
RBK-10 <rbk-10@Mac-mini-2.Dlink>
2019-04-07 04:47:31 +0300

    strinfiy

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,47 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  var str = "";
+
+  if ( obj === undefined || typeof obj === "function" ){
+  	return "";
+  } else if( obj === null ){
+  	str = "null";
+  } else {
+
+  	if ( Array.isArray(obj)) {
+	   	str += "[";
+	    
+	    for ( var k in obj ) {
+	      str += stringifyJSON(obj[k])  ;
+	      if ( k < obj.length - 1 ){
+	      	str += ",";
+	      }
+	    }
+	    str += "]";
+  	} else if ( typeof obj !== "object" ){
+
+  	  	if ( typeof obj === "string" ){
+      		str += "\"" + obj + "\"";
+      	} else { 
+      	 str += obj;
+        }  
+
+    } else {
+    	
+        str += "{";
+        for ( var k in obj ){
+        	if ( k === "undefined" ||  k === "functions" ){
+  					str += "";
+  			} else {
+      	  		str += "\"" + k + "\":" + stringifyJSON( obj[k] )  ;
+       			if ( k != Object.keys( obj )[Object.keys( obj ).length-1] ){
+      				str += ","
+          		}   
+        	}
+      	}
+        str += "}";
+      }
+   }
+  return str;
 };
